da1af5fb45eab41f6f16c432317c969870ec6bee
hardbacknutter <user@localhost>
2024-11-29 12:08:08 +0000

    another go at clarifying the building steps

--- app/src/main/cpp/prebuild-dropbear.sh
+++ app/src/main/cpp/prebuild-dropbear.sh
@@ -6,4 +6,3 @@ cd dropbear/src || { echo "Must be executed from the 'cpp' directory"; exit 1; }
 [ default_options.h -nt default_options_guard.h ] && ./ifndef_wrapper.sh < default_options.h > default_options_guard.h
 
 cd ../..
-
