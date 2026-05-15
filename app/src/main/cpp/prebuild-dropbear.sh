fbf4e438f986a81ed406a4faba186f473221dba9
hardbacknutter <user@localhost>
2026-05-15 09:50:32 +0100

    use subshell context so we can run this script from anywhere

--- app/src/main/cpp/prebuild-dropbear.sh
+++ app/src/main/cpp/prebuild-dropbear.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-cd dropbear/src || { echo "Must be executed from the 'cpp' directory"; exit 1; }
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+SRC_DIR="$SCRIPT_DIR/dropbear/src"
 
-# -nt 	Compares modification time of two files and determines which one is newer.
-[ default_options.h -nt default_options_guard.h ] && ./ifndef_wrapper.sh < default_options.h > default_options_guard.h
+if [ ! -d "$SRC_DIR" ]; then
+    echo "Error: '$SRC_DIR' directory not found."
+    exit 1
+fi
 
-cd ../..
+# Open a subshell context so we can run this script from anywhere
+(
+    cd "$SRC_DIR"
+
+    # -nt Compares modification time of two files and determines which one is newer.
+    if [ default_options.h -nt default_options_guard.h ]; then
+        ./ifndef_wrapper.sh < default_options.h > default_options_guard.h
+    fi
+)
