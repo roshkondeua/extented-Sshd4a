1f603fb0c9e9148d96b48d9585ea06e2ba5a00d0
hardbacknutter <user@localhost>
2026-05-16 11:06:18 +0100

    do NOT rely on 'latest' directory, simply rely on the PATH and complain as needed. Also added missing stderr redirects

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# 2026-05-15: first experiment with an automated build script, use with care
+# 2026-05-16: first experiment with an automated build script, use with care
 
 if [ -z "$ANDROID_HOME" ] || [ ! -d "$ANDROID_HOME" ]; then
-    echo "Error: ANDROID_HOME is not set or the directory does not exist."
-    echo "Please set it first (e.g., export ANDROID_HOME=\$HOME/android-sdk)"
+    echo >&2 "Error: ANDROID_HOME is not set or the directory does not exist."
+    echo >&2 "Please set it first (e.g., export ANDROID_HOME=\$HOME/android-sdk)"
+    exit 1
+fi
+
+if ! command -v sdkmanager >/dev/null 2>&1; then
+    echo >&2 "Error: 'sdkmanager' not found."
+    echo >&2 "Please add your Android SDK 'cmdline-tools/bin' or 'tools/bin' directory to the PATH."
     exit 1
 fi
 
 if [ ! -d "app" ]; then
-    echo "Error: 'app' directory not found."
-    echo "Please execute the build from within the project root directory."
+    echo >&2 "Error: 'app' directory not found."
+    echo >&2 "Please execute the build from within the project root directory."
     exit 1
 fi
 
@@ -25,14 +31,14 @@ fi
 # Extract the cmake version needed
 export CMAKE_VER=$(awk -F'"' '/^cmakeVersion[[:space:]]*=/ {print $2}' gradle/libs.versions.toml)
 if [ -z "$CMAKE_VER" ]; then
-    echo "Error: Could not parse cmakeVersion from libs.versions.toml"
+    echo >&2 "Error: Could not parse cmakeVersion from libs.versions.toml"
     exit 1
 fi
 
 # cmake must be installed manually by sdkmanager
 # TODO: Gradle seems to refuse installing it if there is a global cmake command?
 # Will do nothing if it's already there.
-$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "cmake;$CMAKE_VER"
+sdkmanager "cmake;$CMAKE_VER"
 
 # Add to the path
 export PATH=$PATH:$ANDROID_HOME/cmake/$CMAKE_VER/bin
